c756c1ee739d7d999bca312c64d3bf65a3eaa878
Postnikov Maksim <postnikovmaksim@gmail.com>
2019-01-13 14:13:03 +0300

    filter users

--- eventService.js
+++ eventService.js
@@ -8,12 +8,12 @@ module.exports = {
         const buildTarget = req.body.build_status_url.match(/Box\d\d/g).toString().toLowerCase();
 
         const users = await getUsers();
-
-        users.filter(user => user.options.deploy[buildTarget]).forEach(async user => {
-            const reference = TurnContext.getConversationReference(user.activity);
-            await adapter.continueConversation(reference, async (context) => {
-                await context.sendActivity(`${buildResult} deploy ${buildName} on ${buildTarget}`);
+        users.filter(user => user.options && user.options.deploy && user.options.deploy[buildTarget])
+            .forEach(async user => {
+                const reference = TurnContext.getConversationReference(user.activity);
+                await adapter.continueConversation(reference, async (context) => {
+                    await context.sendActivity(`${buildResult} deploy ${buildName} on ${buildTarget}`);
+                })
             })
-        })
     }
 };
