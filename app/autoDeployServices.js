b286e84c2e18c40996a2b4d05af9645cc1d888f2
Postnikov Maksim <postnikovmaksim@gmail.com>
2019-01-23 22:30:40 +0300

    название комитов в сооьбщение о деплое

--- app/autoDeployServices.js
+++ app/autoDeployServices.js
@@ -2,13 +2,21 @@ const { TurnContext } = require('botbuilder');
 const moment = require('moment');
 const { getActivitys } = require('./userServices');
 const { getUserIds } = require('./subscriptionsServices');
+const { getCommitMessages } = require('./teamCityServices');
 
 module.exports = {
     async autoDeployEvent ({ req, adapter }) {
         const buildDate = moment(req.body.timestamp).format('hh:mm:ss DD.MM.YYYY').add('hours', 3);
         const buildResult = req.body.build_result;
         const buildName = req.body.build_name;
-        const buildTarget = req.body.build_status_url.match(/Box\d\d/g).toString().toLowerCase();
+        const buildNumber = req.body.build_number;
+        const buildStatusUrl = req.body.build_status_url;
+        const buildTarget = buildStatusUrl.match(/Box\d\d/g).toString().toLowerCase();
+        const commitMessages = await getCommitMessages({ buildNumber });
+
+        const message = `${buildDate} ${getStringBuildResult({ buildResult })} ${buildName} на ${buildTarget}` +
+            `\n изменения: ${commitMessages.join(', ')}` +
+            buildResult === 'failed' ? `\n ${buildStatusUrl}` : '';
 
         const ids = await getUserIds({ eventName: `deploy_${buildTarget}` });
         if (!ids.length) {
@@ -20,7 +28,7 @@ module.exports = {
             const reference = TurnContext.getConversationReference(activity);
             await adapter.continueConversation(reference, async (context) => {
                 try {
-                    await context.sendActivity(`${buildDate} ${buildResult} deploy ${buildName} on ${buildTarget}`);
+                    await context.sendActivity(message);
                 } catch (e) {
                     console.log(e);
                 }
@@ -28,3 +36,14 @@ module.exports = {
         })
     }
 };
+
+function getStringBuildResult ({ buildResult }) {
+    switch (buildResult) {
+    case 'success' :
+        return 'успешно выложен';
+    case 'failed' :
+        return 'произошла ошибка при выкладке';
+    default :
+        return '';
+    }
+}
