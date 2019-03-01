879cea7133b3525adc0e5834946a475f72a0aeaa
Postnikov Maksim <postnikovmaksim@gmail.com>
2019-03-01 11:10:59 +0300

    Update zabbixService.js

--- app/eventsServices/zabbixService.js
+++ app/eventsServices/zabbixService.js
@@ -3,52 +3,60 @@ const { sendMessage } = require('../dialogServices');
 const { saveEvent } = require('./commonEventServices');
 
 module.exports = {
-    async zabbixErrorEvent ({ req }) {
+    async zabbixEvent ({ req }) {
         await saveEvent({
             name: 'zabbix_error',
             date: moment().add(3, 'hour').format('YYYY-MM-DD HH:mm:ss'),
             json: JSON.stringify(req.body)
         });
 
-        // const {
-        //     problemResolvedTime,
-        //     problemResolvedDate,
-        //     problemName,
-        //     problemAge,
-        //     host,
-        //     severity,
-        //     tags
-        // } = req.body;
-        //
-        // const message = `Обнаружена проблема ${problemResolvedTime} ${problemResolvedDate}: ${problemName}\n` +
-        // `host: ${host}, severity: ${severity}, tags${JSON.stringify(tags)}, app:${tags.Application}`;
-        //
-        // await sendMessage({ message, eventName: `zabbix_${tags.Application}` });
-        // await sendMessage({ message, eventName: `zabbix_all` });
-    },
+        if (req.body.problemResolvedTime) {
+            await zabbixOkEvent({ req })
+        }
 
-    // async zabbixOkEvent ({ req }) {
-    //     await saveEvent({
-    //         name: 'zabbix_ok',
-    //         date: moment().add(3, 'hour').format('YYYY-MM-DD HH:mm:ss'),
-    //         json: JSON.stringify(req.body)
-    //     });
-    //
-    //     const {
-    //         problemStarted,
-    //         problemDate,
-    //         problemName,
-    //         host,
-    //         severity,
-    //         tags
-    //     } = req.body;
-    //
-    //     const message = `Решена проблемма\n` +
-    //         `дата создания проблемы: ${problemStarted} ${problemDate}\n` +
-    //         `сообщение проблемы: ${problemName}\n` +
-    //         `host: ${host}, severity: ${severity}, tags${JSON.stringify(tags)}, app:${tags.Application}`;
-    //
-    //     await sendMessage({ message, eventName: `zabbix_${tags.Application}` });
-    //     await sendMessage({ message, eventName: `zabbix_all` });
-    // }
+        await zabbixErrorEvent({ req });
+    }
 };
+
+async function zabbixErrorEvent ({ req }) {
+    const {
+        problemResolvedTime,
+        problemResolvedDate,
+        problemName,
+        problemAge,
+        host,
+        severity,
+        tags
+    } = req.body;
+
+    const message = `Обнаружена проблема ${problemResolvedTime} ${problemResolvedDate}: ${problemName}\n` +
+    `host: ${host}, severity: ${severity}, tags${JSON.stringify(tags)}, app:${tags.Application}`;
+
+    await sendMessage({ message, eventName: `zabbix_${tags.Application}` });
+    await sendMessage({ message, eventName: `zabbix_all` });
+}
+
+async function zabbixOkEvent ({ req }) {
+    await saveEvent({
+        name: 'zabbix_ok',
+        date: moment().add(3, 'hour').format('YYYY-MM-DD HH:mm:ss'),
+        json: JSON.stringify(req.body)
+    });
+
+    const {
+        problemStarted,
+        problemDate,
+        problemName,
+        host,
+        severity,
+        tags
+    } = req.body;
+
+    const message = `Решена проблемма\n` +
+        `дата создания проблемы: ${problemStarted} ${problemDate}\n` +
+        `сообщение проблемы: ${problemName}\n` +
+        `host: ${host}, severity: ${severity}, tags${JSON.stringify(tags)}, app:${tags.Application}`;
+
+    await sendMessage({ message, eventName: `zabbix_${tags.Application}` });
+    await sendMessage({ message, eventName: `zabbix_all` });
+}
