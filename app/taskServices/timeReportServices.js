4376c71404c1c523632b3fdc3a01be24aed4304b
Postnikov Maksim <postnikovmaksim@gmail.com>
2019-04-01 15:21:15 +0300

    add time

--- app/taskServices/timeReportServices.js
+++ app/taskServices/timeReportServices.js
@@ -12,7 +12,7 @@ const eventRegx = /timeReport_\S+/;
 
 const username = 'restapi';
 const password = 'aCkko5IQWxRZl3ROtppxRHReCdZMSQDd';
-const sendTime = moment({ hours: 15, minutes: 15 });
+const sendTime = moment({ hours: 15, minutes: 24 }).add(3, 'hour');
 
 const timeReportService = {
     async search ({ context, userId, message }) {
