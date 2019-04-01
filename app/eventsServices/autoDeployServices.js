2bdd1a6650639010bec32bbb2d8d5defae4f1305
Postnikov Maksim <postnikovmaksim@gmail.com>
2019-04-01 22:46:46 +0300

    fix deploy

--- app/eventsServices/autoDeployServices.js
+++ app/eventsServices/autoDeployServices.js
@@ -46,11 +46,16 @@ module.exports = {
 
 function getChanges ({ changesBase64 }) {
     const changes = JSON.parse(new Buffer.from(changesBase64, 'base64').toString('utf8'));
-    return `\n изменения:\n${changes.map(change => {
-        const text = change.search(/\n\n*/) >= 0
-            ? change.match(/.+\n\n*/)[0].replace(/\n\n*/, '')
-            : change.replace(/\n/, '');
+    return `\n изменения:\n${Array.isArray(changes)
+        ? changes.map(change => format(change)).join(`\n`)
+        : format(changes)
+    }`;
+}
 
-        return `-- ${text}`
-    }).join(`\n`)}`;
+function format ({ change }) {
+    const text = change.search(/\n\n*/) >= 0
+        ? change.match(/.+\n\n*/)[0].replace(/\n\n*/, '')
+        : change.replace(/\n/, '');
+
+    return `-- ${text}`
 }
